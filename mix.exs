05813ebf57a8dc0fe1cb47d458f56fe60a2a255c
Sean Callan <seancallan@gmail.com>
2025-08-06 14:00:34 -0400

    chore: GHA + Release Please support

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Cobblestone.MixProject do
   def project do
     [
       app: :cobblestone,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       compilers: [:leex, :yecc] ++ Mix.compilers(),
